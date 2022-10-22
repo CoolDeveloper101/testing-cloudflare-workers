a738b10f7c87cf0b4c705c2c847ab85a4f3b0484
Ashwin Gupta <cooldeveloper101@gmail.com>
2022-10-22 19:41:00 +0530

    Test edge on netlify

--- src/routes/hello/+page.js
+++ src/routes/hello/+page.js
@@ -1,11 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-	try {
-		await fetch('/api/random-number');
-	}
-	catch(err) {
-		return { err };
-	}
+	await fetch('/api/random-number');
 	return {
 		post: {
 			title: `Title for ${params.slug} goes here`,
